574958a01fa11303de11e23059084aa6b86901ab
Mark Dolbyrev <mark.dolbyrev@lcsas-MacBook-Pro-2.local>
2016-08-24 16:38:17 +0300

    Separate modules (core/graphics/libs/console).

--- gulp/tasks/compile.js
+++ gulp/tasks/compile.js
@@ -1,12 +1,14 @@
 var gulp = require("gulp");
 var shell = require('gulp-shell');
+var del = require("del");
 
 gulp.task(
     "compile",
     function (cb) {
         console.log("START! compile.js");
 
-        return gulp.src(".", {read: false})
+        del(["dist/**/*"]).then(
+            gulp.src(".", {read: false})
             .pipe(shell([
                 "tsc"
             ]))
@@ -14,6 +16,7 @@ gulp.task(
                 "end",
                 function () {
                     console.log("FINISH! compile.js");
+                    cb();
                 }
 
             ).on(
@@ -22,75 +25,6 @@ gulp.task(
                     console.log("ERROR! compile.js");
                 }
             )
+        );
     }
 );
-
-
-// // NOTE: gulp-typescript + sourcemaps - more or less workable
-//
-// var gulp = require("gulp");
-// var ts = require("gulp-typescript");
-// var tsProject = ts.createProject("tsconfig.json");
-// var merge2 = require('merge2');
-// var sourcemaps = require('gulp-sourcemaps');
-//
-// console.log("tsProject.options: ");
-// console.dir(tsProject.options);
-// console.dir(" - ");
-//
-// gulp.task(
-//     "compile",
-//     function (cb) {
-//         console.log("START! compile.js");
-//
-//         /*tsProject.src()
-//         .pipe(ts(tsProject))
-//         .js.pipe(gulp.dest("src"))
-//         .on(
-//             "finish",
-//             function() {
-//                 console.log("FINISH! compile.js");
-//                 cb();
-//             }
-//         );*/
-//
-//         var tsResult = tsProject.src()
-//             .pipe(ts(tsProject));
-//
-//         return merge2([
-//             // tsResult.dts.pipe(gulp.dest("./src"))
-//             // ,
-//             tsResult.js.pipe(gulp.dest(""))
-//         ]).on(
-//             "queueDrain",
-//             function() {
-//
-//                 console.log("FINISH! compile.js");
-//
-//                 /*return gulp.src("src/!**!/!*.js")
-//                     .pipe(sourcemaps.init())
-//                     .pipe(sourcemaps.write())
-//                     .pipe(gulp.dest("src"))
-//                     .on(
-//                         "end",
-//                         function() {
-//                             console.log("FINISH! compile.js");
-//                         }
-//                     );*/
-//
-//                 /*gulp.src("src/!**!/!*.js")
-//                     .pipe(sourcemaps.init({identityMap: true}))
-//                     .pipe(sourcemaps.write(".", {includeContent: false, sourceRoot: "./src"}))
-//                     .pipe(gulp.dest("src"))
-//                     .on(
-//                         "end",
-//                         function () {
-//                             console.log("FINISH! compile.js");
-//                         }
-//                     );*/
-//
-//                 // cb();
-//             }
-//         );
-//     }
-// );
